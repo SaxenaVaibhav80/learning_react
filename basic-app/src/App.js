22a4becd5b61eccaa72753d650214cb1115fb12f
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2024-04-30 00:15:26 +0530

    rendering 'Heading' components in react

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Heading from './components/Heading'
+
 function App() {
   return (
     <div className="App">
+     <Heading></Heading>
      <Greet></Greet>
     </div>
   );
---
+++ basic-app/src/components/Heading.js
@@ -0,0 +1,8 @@
+import react from 'react'
+
+function Heading()
+{
+    return <h1>This is Heading</h1>
+}
+
+export default Heading 
