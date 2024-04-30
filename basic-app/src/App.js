71ec14f902b4910d60bb2b0050adda5cfc932eb4
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2024-04-30 13:28:50 +0530

    updating footer

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -1,9 +1,9 @@
 // import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
-import Heading from './components/Heading'
+import Heading from './components/Heading';
 import Paragraph from './components/Paragraph';
-import Footer from './components/Footer.jsx'
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
