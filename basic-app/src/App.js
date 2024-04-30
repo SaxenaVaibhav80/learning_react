4a9fb5d11c54669eaab0899f4cef9f035879abcd
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2024-04-30 11:36:48 +0530

    footer done

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Heading from './components/Heading'
+import Paragraph from './components/Paragraph';
+import Footer from './components/Footer.jsx'
 
 function App() {
   return (
     <div className="App">
      <Heading></Heading>
      <Greet></Greet>
+     <Paragraph></Paragraph>
+     <Footer></Footer>
     </div>
   );
 }
---
+++ basic-app/src/components/Footer.jsx
@@ -0,0 +1,6 @@
+function Footer()
+{
+    return <h1>This is footer and here we endd !!!!!</h1>
+}
+
+export  default Footer 
