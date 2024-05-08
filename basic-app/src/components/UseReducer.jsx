5e6187fc481ea6bdc671e3ad978cc673fdbc6dc1
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2024-05-09 04:01:54 +0530

    updating some useReducer hook's documntation

--- basic-app/src/components/UseReducer.jsx
+++ basic-app/src/components/UseReducer.jsx
@@ -2,31 +2,62 @@
 import { useReducer } from "react"; 
 
 
-function UseReducer()
-{
-    const initialstate=0
-    const reducer = (state,action)=>
-        {
-           switch(action)
-           {
-             case "Increment":
-                return state=state+1
-             case "Decrement":
-                return state=state-1
-             default:
-                return state
-            }
+// function UseReducer()
+// {
+//     const initialstate=0
+//     const reducer = (state,action)=>
+//         {
+//            switch(action)
+//            {
+//              case "Increment":
+//                 return state=state+1
+//              case "Decrement":
+//                 return state=state-1
+//              default:
+//                 return state
+//             }
+//         }
+//         const [count,dispatch]= useReducer(reducer,initialstate)
+//         console.log(useReducer(reducer,initialstate))
+//     return(
+//        <>
+//          <h1>{count}</h1>
+//          <button onClick={()=>dispatch("Incremet")}>increment</button>
+//          <button onClick={()=>dispatch("Decrement")}>decrement</button>
+//        </>
+//     )
+// }
+
+
+
+function UseReducer() {
+    const initialstate = 0;
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "INCREMENT":
+                return state + 1;
+            case "DECREMENT":
+                return state - 1;
+            default:
+                return state;
         }
-        const [count,dispatch]= useReducer(reducer,initialstate)
-        console.log(useReducer(reducer,initialstate))
-    return(
-       <>
-         <h1>{count}</h1>
-         <button onClick={()=>dispatch("Incremet")}>increment</button>
-         <button onClick={()=>dispatch("Decrement")}>decrement</button>
-       </>
-    )
+    };
+
+    const [count, dispatch] = useReducer(reducer, initialstate);
+
+    return (
+        <>
+            <h1>{count}</h1>
+            <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
+            <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</button>
+        </>
+    );
 }
+
+// Note-> this is the goood practice means action are store in the js object with "type" property 
+
+export default UseReducer;
+
 
 /*ABOUT useReducer---->
 
@@ -42,4 +73,3 @@ preferable for complex state management logic or to hadle multiple states */
 
 // IF WE CAN NOT PASS REDUCER Function IN THE useReducer then it will not return any function so you will store undefined in the dipatch variable to avoid this you need to pass reducer in the useReducer
 
-export default UseReducer
